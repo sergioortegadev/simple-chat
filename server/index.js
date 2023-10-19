6cc3275ebab57f4267a922e281d37b6dbfa0acc4
Sergio Ortega <instintoimagen@gmail.com>
2023-10-19 17:43:53 -0300

    Td0001 - Proyecto inicial ok

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import logger from "morgan";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
+
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log(` + User conectado +`);
+
+  socket.on("disconnect", () => {
+    console.log(" - Usuario desconectado -");
+  });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+app.use(logger("dev"));
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/client/index.html");
+});
+
+server.listen(port, () => {
+  console.log(`▓▒ Server funcionando en puerto ${port} ▒▓`);
+});
